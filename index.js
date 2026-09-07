2175bcef954992aa31e6ed2e7b58a2b9290e7003
Lễ Nguyễn <nguyenbale29@gmail.com>
2026-09-06 17:46:25 -0700

    Added job listing page with working API call

--- index.js
+++ index.js
@@ -21,16 +21,35 @@ app.post("/submit", async (req, res) => {
     console.log(req.body);
     const job_title = req.body.job_Title;
     const job_country = req.body.job_Country;
+    const post_age = req.post_Age;
 
     try {
         const result = await axios.get(`https://api.adzuna.com/v1/api/jobs/${job_country}/search/1`, {
             params: {
                 app_id: "3b3f62bc",
                 app_key: "88faf6ea39c17d6e714c4ae6f70c2f4d",
-                what: job_title
+                what: job_title,
+                max_days_old: post_age,
+                results_per_page: 30
             }
         });
-        console.log(result.data)
+
+        const job_posts = result.data.results;
+        var job_list = [];
+
+        job_posts.forEach(job => {
+            const location_str = `${job.location.display_name}, ${job.location.area[1]}, ${job.location.area[0]}`;
+            job_list.push({
+                "title": job.title,
+                "company": job.company.display_name,
+                "location": location_str,
+                "created": new Date(job.created).toISOString().split('T')[0],
+                "description": job.description,
+                "redirect_url": job.redirect_url,
+            });
+        });
+
+        res.render("job_listing.ejs", {listings: job_list});
     } catch (error) {
         console.log(error.message)
     }
